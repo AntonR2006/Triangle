0b5313b38bfd8753d66edcd9d1f1679da627e20f
AntonR2006 <42034495+AntonR2006@users.noreply.github.com>
2018-08-02 15:11:28 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,73 @@
+====================================
+STATION PARTS EXPANSION REDUX v1.0.3
+====================================
+
+This pack provides a set of new space and ground station parts. New parts exist in all size classes from 1.25m to 3.75m. Rigid station parts are provided as well as expandable centrifuges and habitats. Ground bases are served with auto-leveling bases and extendable docking crew tubes.
+In addition this pack reskins the Hitchiker, Cupola and Mobile Lab to improve their quality and match the new parts.
+
+============
+DEPENDENCIES
+============
+
+Required:
+- ModuleManager (3.0.6)
+- B9PartSwitch (2.2.1)
+- NearFutureProps (0.3.2)
+
+Optional
+- RescuePodValidator
+- Community Tech Tree (v2.2+)
+
+=============
+EXTRA CREDITS
+=============
+
+Huge thanks to the following forum users:
+- JadeofMaar (KIS, EPL, TAC-LS, Snacks! and multi-container patches),
+- Stone Blue (IVA prop layouts in several parts)
+- fraz86 (KerbalHealth patch)
+- Pulsar (USI-LS/MKS patches)
+
+============
+INSTALLATION
+============
+
+To install, place the GameData folder inside your Kerbal Space Program folder. If asked to overwrite files, do so.
+
+=============
+COMPATIBILITY
+=============
+
+This mod contains explicit compatibility patches for the following mods:
+- TAC-LS
+- USI-LS
+- MKS
+- KerbalHealth
+- KIS
+- CLS
+- Snacks!
+- KeepFit
+
+============
+KSP-MINI-AVC
+============
+
+This mod includes version checking using MiniAVC (http://forum.kerbalspaceprogram.com/threads/79745).
+If you opt-in, it will use the internet to check whether there is a new version available. Data is only read from the internet and no personal information is sent.
+For a more comprehensive version checking experience, please download the KSP-AVC Plugin (http://forum.kerbalspaceprogram.com/threads/79745).
+
+============
+LOCALIZATION
+============
+
+This mod includes localization support, and includes translations for
+-
+
+=========
+LICENSING
+=========
+
+The cfgs and code in this pack are distributed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License (http://creativecommons.org/licenses/by-nc-sa/4.0/legalcode). You are free to share and adapt the materials only for non-commercial purposes and when providing appropriate attribution. Any derivatives must be distributed under the same license.
+The art assets in this pack (all models and textures) are distributed under an All Rights Reserved License. You may not redistribute or re-use these assets without express permission from me.
+
+ModuleManager by ialdabaoth and sarbian is distributed under a Creative Commons Sharealike license. More details, including source code, can be found here: http://forum.kerbalspaceprogram.com/threads/31342-0-20-ModuleManager-1-3-for-all-your-stock-modding-needs?p=528607&viewfull=1#post528607
